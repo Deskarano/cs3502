db4b452e85c30c8c10f316d5b468cf37468cd59f
Gregor Haas <gregorhaas1997@gmail.com>
2018-02-08 13:24:06 -0500

    Implemented all instructions that do not access RAM

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -65,7 +65,12 @@ void execute(unsigned int instruction, unsigned int reg[16], unsigned int *pc)
 
     if(op == 0x08) //DIV
     {
+        int sreg1 = (instruction >> 20) & 0b1111;
+        int sreg2 = (instruction >> 16) & 0b1111;
+        int dreg = (instruction >> 12) & 0b1111;
 
+        reg[dreg] = reg[sreg1] / reg[sreg2];
+        return;
     }
 
     if(op == 0x09) //AND
@@ -75,6 +80,7 @@ void execute(unsigned int instruction, unsigned int reg[16], unsigned int *pc)
         int dreg = (instruction >> 12) & 0b1111;
 
         reg[dreg] = reg[sreg1] & reg[sreg2];
+        return;
     }
 
     if(op == 0x0A) //OR
@@ -84,46 +90,69 @@ void execute(unsigned int instruction, unsigned int reg[16], unsigned int *pc)
         int dreg = (instruction >> 12) & 0b1111;
 
         reg[dreg] = reg[sreg1] | reg[sreg2];
+        return;
     }
 
     if(op == 0x0B) //MOVI
     {
+        int dreg = (instruction >> 16) & 0b1111;
+        unsigned int val = (instruction) & 0b00000000000000001111111111111111;
 
+        reg[dreg] = val;
+        return;
     }
 
     if(op == 0x0C) //ADDI
     {
+        int dreg = (instruction >> 16) & 0b1111;
+        unsigned int val = (instruction) & 0b00000000000000001111111111111111;
 
+        reg[dreg] += val;
+        return;
     }
 
     if(op == 0x0D) //MULI
     {
-
+        //not implemented. instruction is never used
+        return;
     }
 
     if(op == 0x0E) //DIVI
     {
-
+        //not implemented. instruction is never used
+        return;
     }
 
     if(op == 0x0F) //LDI
     {
+        int dreg = (instruction >> 16) & 0b1111;
+        unsigned int val = (instruction) & 0b00000000000000001111111111111111;
 
+        reg[dreg] = val;
+        return;
     }
 
     if(op == 0x10) //SLT
     {
+        int sreg1 = (instruction >> 20) & 0b1111;
+        int sreg2 = (instruction >> 16) & 0b1111;
+        int dreg = (instruction >> 12) & 0b1111;
 
+        if(reg[sreg1] < reg[sreg2]) reg[dreg] = 1;
+        else reg[dreg] = 0;
+        return;
     }
 
     if(op == 0x11) //SLTI
     {
-
+        //not implemented. instruction is never used
+        return;
     }
 
     if(op == 0x12) //HLT
     {
-
+        //TODO: implement this. pass PCB to execute()?
+        return;
     }
 
     if(op == 0x13) //NOP
@@ -141,32 +170,52 @@ void execute(unsigned int instruction, unsigned int reg[16], unsigned int *pc)
 
     if(op == 0x15) //BEQ
     {
+        int breg = (instruction >> 20) & 0b1111;
+        int dreg = (instruction >> 16) & 0b1111;
+        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
 
+        if(reg[breg] == reg[dreg]) *pc = addr;
     }
 
     if(op == 0x16) //BNE
     {
+        int breg = (instruction >> 20) & 0b1111;
+        int dreg = (instruction >> 16) & 0b1111;
+        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
 
+        if(reg[breg] != reg[dreg]) *pc = addr;
     }
 
     if(op == 0x17) //BEZ
     {
+        int breg = (instruction >> 20) & 0b1111;
+        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
 
+        if(reg[breg] == 0) *pc = addr;
     }
 
     if(op == 0x18) //BNZ
     {
+        int breg = (instruction >> 20) & 0b1111;
+        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
 
+        if(reg[breg] != 0) *pc = addr;
     }
 
     if(op == 0x19) //BGZ
     {
+        int breg = (instruction >> 20) & 0b1111;
+        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
 
+        if(reg[breg] > 0) *pc = addr;
     }
 
     if(op == 0x1A) //BLZ
     {
+        int breg = (instruction >> 20) & 0b1111;
+        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
 
+        if(reg[breg] < 0) *pc = addr;
     }
 }
 
