d94bcdaf56ad4ebb7026964571ab06a24554d3dd
Gregor Haas <gregorhaas1997@gmail.com>
2018-02-10 16:50:14 -0500

    Improved CPU internal structure

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "cpu.h"
+#include "cpu_types.h"
 
 void copy_reg(const unsigned int from[16], unsigned int to[16])
 {
@@ -9,286 +10,380 @@ void copy_reg(const unsigned int from[16], unsigned int to[16])
     }
 }
 
-void execute(unsigned int instruction, unsigned int reg[16], unsigned int *pc)
+void execute(instr *instruction, unsigned int reg[16], unsigned int *pc)
 {
+    switch(instruction->op)
+    {
+        case RD:
+        {
+            auto args = (io_args *) instruction->args;
+            if(args->reg2 == 0)
+            {
+                reg[args->reg1] = cpu_test_ram::read_word(args->addr);
+            }
+            else
+            {
+                reg[args->reg1] = cpu_test_ram::read_word(reg[args->reg2]);
+            }
+
+            *pc += 4;
+            return;
+        }
+
+        case WR:
+        {
+            auto args = (io_args *) instruction->args;
+            if(args->reg2 == 0)
+            {
+                cpu_test_ram::write_word(args->addr, reg[args->reg1]);
+            }
+            else
+            {
+                cpu_test_ram::write_word(reg[args->reg2], reg[args->reg1]);
+            }
+
+            *pc += 4;
+            return;
+        }
+
+        case ST:
+        {
+            auto args = (i_args *) instruction->args;
+            cpu_test_ram::write_word(reg[args->dreg], reg[args->breg]);
+
+            *pc += 4;
+            return;
+        }
+
+        case LW:
+        {
+            auto args = (i_args *) instruction->args;
+            reg[args->dreg] = cpu_test_ram::read_word(reg[args->breg] + args->addr);
+
+            *pc += 4;
+            return;
+        }
+
+        case MOV:
+        {
+            auto args = (r_args *) instruction->args;
+            reg[args->sreg1] = reg[args->sreg2];
+
+            *pc += 4;
+            return;
+        }
+
+        case ADD:
+        {
+            auto args = (r_args *) instruction->args;
+            reg[args->dreg] = reg[args->sreg1] + reg[args->sreg2];
+
+            *pc += 4;
+            return;
+        }
+
+        case SUB:
+        {
+            //not implemented. instruction is never used
+            *pc += 4;
+            return;
+        }
+
+        case MUL:
+        {
+            //not implemented. instruction is never used
+            *pc += 4;
+            return;
+        }
+
+        case DIV:
+        {
+            auto args = (r_args *) instruction->args;
+            reg[args->dreg] = reg[args->sreg1] / reg[args->sreg2];
+
+            *pc += 4;
+            return;
+        }
+
+        case AND:
+        {
+            auto args = (r_args *) instruction->args;
+            reg[args->dreg] = reg[args->sreg1] & reg[args->sreg2];
+
+            *pc += 4;
+            return;
+        }
+
+        case OR:
+        {
+            auto args = (r_args *) instruction->args;
+            reg[args->dreg] = reg[args->sreg1] | reg[args->sreg2];
+
+            *pc += 4;
+            return;
+        }
+
+        case MOVI:
+        {
+            auto args = (i_args *) instruction->args;
+            reg[args->dreg] = args->addr;
+
+            *pc += 4;
+            return;
+        }
+
+        case ADDI:
+        {
+            auto args = (i_args *) instruction->args;
+            reg[args->dreg] += args->addr;
+
+            *pc += 4;
+            return;
+        }
+
+        case MULI:
+        {
+            //not implemented. instruction is never used
+            *pc += 4;
+            return;
+        }
+
+        case DIVI:
+        {
+            //not implemented. instruction is never used
+            *pc += 4;
+            return;
+        }
+
+        case LDI:
+        {
+            auto args = (i_args *) instruction->args;
+            reg[args->dreg] = args->addr;
+
+            *pc += 4;
+            return;
+        }
+
+        case SLT:
+        {
+            auto args = (r_args *) instruction->args;
+            if(reg[args->sreg1] < reg[args->sreg2])
+            {
+                reg[args->dreg] = 1;
+            }
+            else
+            {
+                reg[args->dreg] = 0;
+            }
+
+            *pc += 4;
+            return;
+        }
+
+        case SLTI:
+        {
+            //not implemented. instruction is never used
+            *pc += 4;
+            return;
+        }
+
+        case HLT:
+        {
+            //TODO: implement this. pass PCB to execute()?
+            return;
+        }
+
+        case NOP:
+        {
+            *pc += 4;
+            return;;
+        }
+
+        case JMP:
+        {
+            auto args = (j_args *) instruction->args;
+            *pc = args->addr;
+
+            return;
+        }
+
+        case BEQ:
+        {
+            auto args = (i_args *) instruction->args;
+            if(reg[args->breg] == reg[args->dreg])
+            {
+                *pc = args->addr;
+            }
+            else
+            {
+                *pc += 4;
+            }
+
+            return;
+        }
+
+        case BNE:
+        {
+            auto args = (i_args *) instruction->args;
+            if(reg[args->breg] != reg[args->dreg])
+            {
+                *pc = args->addr;
+            }
+            else
+            {
+                *pc += 4;
+            }
+
+            return;
+        }
+
+        case BEZ:
+        {
+            auto args = (i_args *) instruction->args;
+            if(reg[args->breg] == 0)
+            {
+                *pc = args->addr;
+            }
+            else
+            {
+                *pc += 4;
+            }
+
+            return;
+        }
+
+        case BNZ:
+        {
+            auto args = (i_args *) instruction->args;
+            if(reg[args->breg] != 0)
+            {
+                *pc = args->addr;
+            }
+            else
+            {
+                *pc += 4;
+            }
+
+            return;
+        }
+
+        case BGZ:
+        {
+            auto args = (i_args *) instruction->args;
+            if(reg[args->breg] > 0)
+            {
+                *pc = args->addr;
+            }
+            else
+            {
+                *pc += 4;
+            }
+
+            return;
+        }
+
+        case BLZ:
+        {
+            auto args = (i_args *) instruction->args;
+            if(reg[args->breg] < 0)
+            {
+                *pc = args->addr;
+            }
+            else
+            {
+                *pc += 4;
+            }
+
+            return;
+        }
+
+        case INVALID:
+        {
+
+        }
+    }
+}
+
+instr *decode(unsigned int instruction)
+{
+    auto result = new instr;
+    int type = (instruction >> 30) & 0b11;
+
+    if(type == 0b00)
+    {
+        result->type = R;
+
+        auto args = new r_args;
+        args->sreg1 = (instruction >> 20) & 0b1111;
+        args->sreg2 = (instruction >> 16) & 0b1111;
+        args->dreg = (instruction >> 12) & 0b1111;
+
+        result->args = args;
+    }
+    else if(type == 0b01)
+    {
+        result->type = I;
+
+        auto args = new i_args;
+        args->breg = (instruction >> 20) & 0b1111;
+        args->dreg = (instruction >> 16) & 0b1111;
+        args->addr = instruction & 0b1111111111111111;
+
+        result->args = args;
+    }
+    else if(type == 0b10)
+    {
+        result->type = J;
+
+        auto args = new j_args;
+        args->addr = instruction & 0b111111111111111111111111;
+
+        result->args = args;
+    }
+    else
+    {
+        result->type = IO;
+
+        auto args = new io_args;
+        args->reg1 = (instruction >> 20) & 0b1111;
+        args->reg2 = (instruction >> 16) & 0b1111;
+        args->addr = instruction & 0b1111111111111111;
+
+        result->args = args;
+    }
+
     int op = (instruction >> 24) & 0b00111111;
 
-    if(op == 0x00) //RD
-    {
-        int reg1 = (instruction >> 20) & 0b1111;
-        int reg2 = (instruction >> 16) & 0b1111;
-        int addr = instruction & 0b00000000000000001111111111111111;
+    if(op == 0x00) result->op = RD;
+    else if(op == 0x01) result->op = WR;
+    else if(op == 0x02) result->op = ST;
+    else if(op == 0x03) result->op = LW;
+    else if(op == 0x04) result->op = MOV;
+    else if(op == 0x05) result->op = ADD;
+    else if(op == 0x06) result->op = SUB;
+    else if(op == 0x07) result->op = MUL;
+    else if(op == 0x08) result->op = DIV;
+    else if(op == 0x09) result->op = AND;
+    else if(op == 0x0A) result->op = OR;
+    else if(op == 0x0B) result->op = MOVI;
+    else if(op == 0x0C) result->op = ADDI;
+    else if(op == 0x0D) result->op = MULI;
+    else if(op == 0x0E) result->op = DIVI;
+    else if(op == 0x0F) result->op = LDI;
+    else if(op == 0x10) result->op = SLT;
+    else if(op == 0x11) result->op = SLTI;
+    else if(op == 0x12) result->op = HLT;
+    else if(op == 0x13) result->op = NOP;
+    else if(op == 0x14) result->op = JMP;
+    else if(op == 0x15) result->op = BEQ;
+    else if(op == 0x16) result->op = BNE;
+    else if(op == 0x17) result->op = BEZ;
+    else if(op == 0x18) result->op = BNZ;
+    else if(op == 0x19) result->op = BGZ;
+    else if(op == 0x1A) result->op = BLZ;
+    else result->op = INVALID;
 
-        if(reg2 == 0)
-        {
-            reg[reg1] = cpu_test_ram::read_word(addr);
-        }
-        else
-        {
-            reg[reg1] = cpu_test_ram::read_word(reg[reg2]);
-        }
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x01) //WR
-    {
-        int reg1 = (instruction >> 20) & 0b1111;
-        int reg2 = (instruction >> 16) & 0b1111;
-        int addr = instruction & 0b00000000000000001111111111111111;
-
-        if(reg2 == 0)
-        {
-            cpu_test_ram::write_word(addr, reg[reg1]);
-        }
-        else
-        {
-            cpu_test_ram::write_word(reg[reg2], reg[reg1]);
-        }
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x02) //ST
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        int dreg = (instruction >> 16) & 0b1111;
-
-        cpu_test_ram::write_word(reg[dreg], reg[breg]);
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x03) //LW
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        int dreg = (instruction >> 16) & 0b1111;
-        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
-
-        reg[dreg] = cpu_test_ram::read_word(reg[breg] + addr);
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x04) //MOV
-    {
-        int sreg1 = (instruction >> 20) & 0b1111;
-        int sreg2 = (instruction >> 16) & 0b1111;
-
-        reg[sreg1] = reg[sreg2];
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x05) //ADD
-    {
-        int sreg1 = (instruction >> 20) & 0b1111;
-        int sreg2 = (instruction >> 16) & 0b1111;
-        int dreg = (instruction >> 12) & 0b1111;
-
-        reg[dreg] = reg[sreg1] + reg[sreg2];
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x06) //SUB
-    {
-        //not implemented. instruction is never used
-        return;
-    }
-
-    if(op == 0x07) //MUL
-    {
-        //not implemented. instruction is never used
-        return;
-    }
-
-    if(op == 0x08) //DIV
-    {
-        int sreg1 = (instruction >> 20) & 0b1111;
-        int sreg2 = (instruction >> 16) & 0b1111;
-        int dreg = (instruction >> 12) & 0b1111;
-
-        reg[dreg] = reg[sreg1] / reg[sreg2];
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x09) //AND
-    {
-        int sreg1 = (instruction >> 20) & 0b1111;
-        int sreg2 = (instruction >> 16) & 0b1111;
-        int dreg = (instruction >> 12) & 0b1111;
-
-        reg[dreg] = reg[sreg1] & reg[sreg2];
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x0A) //OR
-    {
-        int sreg1 = (instruction >> 20) & 0b1111;
-        int sreg2 = (instruction >> 16) & 0b1111;
-        int dreg = (instruction >> 12) & 0b1111;
-
-        reg[dreg] = reg[sreg1] | reg[sreg2];
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x0B) //MOVI
-    {
-        int dreg = (instruction >> 16) & 0b1111;
-        unsigned int val = (instruction) & 0b00000000000000001111111111111111;
-
-        reg[dreg] = val;
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x0C) //ADDI
-    {
-        int dreg = (instruction >> 16) & 0b1111;
-        unsigned int val = (instruction) & 0b00000000000000001111111111111111;
-
-        reg[dreg] += val;
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x0D) //MULI
-    {
-        //not implemented. instruction is never used
-        return;
-    }
-
-    if(op == 0x0E) //DIVI
-    {
-        //not implemented. instruction is never used
-        return;
-    }
-
-    if(op == 0x0F) //LDI
-    {
-        int dreg = (instruction >> 16) & 0b1111;
-        unsigned int val = (instruction) & 0b00000000000000001111111111111111;
-
-        reg[dreg] = val;
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x10) //SLT
-    {
-        int sreg1 = (instruction >> 20) & 0b1111;
-        int sreg2 = (instruction >> 16) & 0b1111;
-        int dreg = (instruction >> 12) & 0b1111;
-
-        if(reg[sreg1] < reg[sreg2]) reg[dreg] = 1;
-        else reg[dreg] = 0;
-
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x11) //SLTI
-    {
-        //not implemented. instruction is never used
-        return;
-    }
-
-    if(op == 0x12) //HLT
-    {
-        //TODO: implement this. pass PCB to execute()?
-        return;
-    }
-
-    if(op == 0x13) //NOP
-    {
-        *pc += 4;
-        return;
-    }
-
-    if(op == 0x14) //JMP
-    {
-        unsigned int target_addr = instruction & 0b00000000111111111111111111111111;
-        *pc = target_addr;
-
-        return;
-    }
-
-    if(op == 0x15) //BEQ
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        int dreg = (instruction >> 16) & 0b1111;
-        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
-
-        if(reg[breg] == reg[dreg]) *pc = addr;
-        return;
-    }
-
-    if(op == 0x16) //BNE
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        int dreg = (instruction >> 16) & 0b1111;
-        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
-
-        if(reg[breg] != reg[dreg]) *pc = addr;
-        else *pc += 4;
-        return;
-    }
-
-    if(op == 0x17) //BEZ
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
-
-        if(reg[breg] == 0) *pc = addr;
-        else *pc += 4;
-        return;
-    }
-
-    if(op == 0x18) //BNZ
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
-
-        if(reg[breg] != 0) *pc = addr;
-        else *pc += 4;
-        return;
-    }
-
-    if(op == 0x19) //BGZ
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
-
-        if(reg[breg] > 0) *pc = addr;
-        else *pc += 4;
-        return;
-    }
-
-    if(op == 0x1A) //BLZ
-    {
-        int breg = (instruction >> 20) & 0b1111;
-        unsigned int addr = (instruction) & 0b00000000000000001111111111111111;
-
-        if(reg[breg] < 0) *pc = addr;
-        else *pc += 4;
-        return;
-    }
+    return result;
 }
 
 void print_registers(unsigned int reg[16])
@@ -301,13 +396,12 @@ void print_registers(unsigned int reg[16])
 
 void cpu::start()
 {
-    unsigned int instruction = cpu_test_ram::read_word(pc);
+    instr *instruction = decode(cpu_test_ram::read_word(pc));
 
-    while(((instruction >> 24) & 0b00111111) != 0x12)
+    while(instruction->op != HLT)
     {
         execute(instruction, reg, &pc);
-
-        instruction = cpu_test_ram::read_word(pc);
+        instruction = decode(cpu_test_ram::read_word(pc));
     }
 }
 
---
+++ cpu/cpu_types.h
@@ -0,0 +1,53 @@
+#ifndef CS3502_CPU_TYPES_H
+#define CS3502_CPU_TYPES_H
+
+enum instr_type
+{
+    R, I, J, IO
+};
+
+enum instr_op
+{
+    RD, WR, ST, LW,
+    MOV, ADD, SUB, MUL, DIV, AND, OR,
+    MOVI, ADDI, MULI, DIVI, LDI,
+    SLT, SLTI,
+    HLT, NOP, JMP,
+    BEQ, BNE, BEZ, BNZ, BGZ, BLZ,
+    INVALID
+};
+
+struct r_args
+{
+    unsigned int sreg1;
+    unsigned int sreg2;
+    unsigned int dreg;
+};
+
+struct i_args
+{
+    unsigned int breg;
+    unsigned int dreg;
+    unsigned int addr;
+};
+
+struct j_args
+{
+    unsigned int addr;
+};
+
+struct io_args
+{
+    unsigned int reg1;
+    unsigned int reg2;
+    unsigned int addr;
+};
+
+typedef struct instr
+{
+    instr_type type;
+    instr_op op;
+    void *args;
+};
+
+#endif //CS3502_CPU_TYPES_H
