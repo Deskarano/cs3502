21cf01b29b9dcb99546988399a6e8a125d79119b
Zach Starnes <zachstarnes@gmail.com>
2018-02-15 12:50:21 -0500

    Update loader.cpp

--- scheduler/loader.cpp
+++ scheduler/loader.cpp
@@ -1,1 +1,2 @@
-// loader goes here
+// loader goes here copy from other branch. pulling from file to disk. parse file while has next line load to disk. make pcb (record first location on disk and the length)
+// translate into binary translate hex into unsigned int then saving to disk
