09965c6d4c65882b1c76b4aca75d31838e88b780
Drew <drew@savas.name>
2018-02-18 12:04:40 -0500

    Made short term files

--- scheduler/short-term.cpp
+++ scheduler/short-term.cpp
@@ -2,3 +2,7 @@
 // Created by adam pellegrini on 2/1/2018.
 //
 
+#include "short-term.h"
+#include "../ram/ram.h"
+
+
---
+++ scheduler/short-term.h
@@ -0,0 +1,29 @@
+//
+// Created by drew on 2/17/2018.
+//
+
+#ifndef CS3502_SHORT_TERM_H
+#define CS3502_SHORT_TERM_H
+
+#include "../cpu/pcb.h"
+
+enum sched_algo
+{
+    FCFS,           //first come first serve
+    PRI,            //priority
+    SJF,            //shortest job first
+    SRT,            //shortest remaining time
+    RR              //round-robin
+};
+
+class shortterm
+{
+public:
+    void add_process(pcb* process);         //used to add a new process to queue
+    void remove_process(pcb* process);
+
+private:
+    pcb* get_next_process(sched_algo sa);
+};
+
+#endif //CS3502_SHORT_TERM_H
