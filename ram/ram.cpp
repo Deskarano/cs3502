7f9d420f0479faeea03ca6d969bc8c2b83a517f7
Gregor Haas <gregorhaas1997@gmail.com>
2018-02-20 12:33:14 -0500

    Updated RAM to store hex characters instead of binary

--- ram/ram.cpp
+++ ram/ram.cpp
@@ -1,59 +1,30 @@
 #include <iostream>
 #include "ram.h"
 
-unsigned int ram::num_bytes = 0;
-unsigned char *ram::data = nullptr;
+unsigned int ram::num_words = 0;
+char *ram::data = nullptr;
 
-void ram::init(unsigned int num_bytes)
+void ram::init(unsigned int num_words)
 {
-    ram::num_bytes = num_bytes;
-    ram::data = new unsigned char[num_bytes];
+    ram::num_words = num_words;
+    ram::data = new char[num_words * 8];
 }
 
-void ram::write_byte(unsigned int addr, unsigned char val)
+void ram::write_word(unsigned int addr, char val[8])
 {
-    if(addr < num_bytes)
+    for(int i = 0; i < 8; i++)
     {
-        data[addr] = val;
-    }
-    else
-    {
-        std::cout << "--ram-error (write_byte): address " << std::hex << addr << " is out of range\n";
+        data[i] = val[i];
     }
 }
 
-unsigned char ram::read_byte(unsigned int addr)
+char *ram::read_word(unsigned int addr)
 {
-    if(addr < num_bytes)
+    auto result = new char[8];
+    for(int i = 0; i < 8; i++)
     {
-        return data[addr];
+        result[i] = data[i];
     }
-    else
-    {
-        std::cout << "--ram-error (read_byte): address " << std::hex << addr << " is out of range\n";
-        return 0;
-    }
-}
 
-void ram::write_word(unsigned int addr, int val)
-{
-    auto val1 = (unsigned char) ((val >> 24) & 0b11111111);
-    auto val2 = (unsigned char) ((val >> 16) & 0b11111111);
-    auto val3 = (unsigned char) ((val >> 8) & 0b11111111);
-    auto val4 = (unsigned char) (val & 0b11111111);
-
-    write_byte(addr, val1);
-    write_byte(addr + 1, val2);
-    write_byte(addr + 2, val3);
-    write_byte(addr + 3, val4);
-}
-
-int ram::read_word(unsigned int addr)
-{
-    int val1 = read_byte(addr);
-    int val2 = read_byte(addr + 1);
-    int val3 = read_byte(addr + 2);
-    int val4 = read_byte(addr + 3);
-
-    return (val1 << 24) + (val2 << 16) + (val3 << 8) + val4;
+    return result;
 }
--- ram/ram.h
+++ ram/ram.h
@@ -4,17 +4,14 @@
 struct ram
 {
 public:
-    static void init(unsigned int num_bytes);
+    static void init(unsigned int num_words);
 
-    static void write_word(unsigned int addr, int val);
-    static int read_word(unsigned int addr);
+    static void write_word(unsigned int addr, char val[8]);
+    static char *read_word(unsigned int addr);
 
 private:
-    static void write_byte(unsigned int addr, unsigned char val);
-    static unsigned char read_byte(unsigned int addr);
-
-    static unsigned int num_bytes;
-    static unsigned char *data;
+    static unsigned int num_words;
+    static char *data;
 };
 
 #endif //CS3502_RAM_H
