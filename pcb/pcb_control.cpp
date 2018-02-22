159db9ce1e32883c84bec44d1873aa4cd1e18c49
Gregor Haas <gregorhaas1997@gmail.com>
2018-02-22 11:00:52 -0500

    Fixed CMakeLists

--- pcb/pcb_control.cpp
+++ pcb/pcb_control.cpp
@@ -6,10 +6,10 @@
 class pcb_node
 {
 public:
-    pcb_node(pcb *pcb)
+    explicit pcb_node(pcb *new_pcb)
     {
         this->next = nullptr;
-        this->pcb = pcb;
+        this->pcb = new_pcb;
     }
 
     pcb_node *get_next()
@@ -32,21 +32,21 @@ unsigned int pcb_control::create_pcb(std::string *job_section)
 {
     unsigned int ID, priority, code_size;
 
-    unsigned long next_space = job_section->find(' ');
+    unsigned int next_space = (int) job_section->find(' ');
     job_section->erase(0, next_space + 1);
-    next_space = job_section->find(' ');
+    next_space = (int) job_section->find(' ');
 
     job_section->erase(0, next_space + 1);
-    next_space = job_section->find(' ');
+    next_space = (int) job_section->find(' ');
     ID = hex_to_dec(job_section->substr(0, next_space).c_str(), next_space);
 
     job_section->erase(0, next_space + 1);
-    next_space = job_section->find(' ');
+    next_space = (int) job_section->find(' ');
     code_size = hex_to_dec(job_section->substr(0, next_space).c_str(), next_space);
 
     job_section->erase(0, next_space + 1);
-    next_space = job_section->find(' ');
-    priority = hex_to_dec(job_section->substr(0, next_space).c_str(), job_section->size() - 1);
+    next_space = (int) job_section->find(' ');
+    priority = hex_to_dec(job_section->substr(0, next_space).c_str(), (unsigned int) job_section->size() - 1);
 
     if(pcb_list_head == nullptr)
     {
@@ -70,21 +70,21 @@ void pcb_control::update_pcb(unsigned int ID, std::string *data_section)
 {
     unsigned int input_size, output_size, temp_size;
 
-    unsigned long next_space = data_section->find(' ');
+    unsigned int next_space = (int) data_section->find(' ');
     data_section->erase(0, next_space + 1);
-    next_space = data_section->find(' ');
+    next_space = (int) data_section->find(' ');
 
     data_section->erase(0, next_space + 1);
-    next_space = data_section->find(' ');
+    next_space = (int) data_section->find(' ');
     input_size = hex_to_dec(data_section->substr(0, next_space).c_str(), next_space);
 
     data_section->erase(0, next_space + 1);
-    next_space = data_section->find(' ');
+    next_space = (int) data_section->find(' ');
     output_size = hex_to_dec(data_section->substr(0, next_space).c_str(), next_space);
 
     data_section->erase(0, next_space + 1);
-    next_space = data_section->find(' ');
-    temp_size = hex_to_dec(data_section->substr(0, next_space).c_str(), data_section->size() - 1);
+    next_space = (int) data_section->find(' ');
+    temp_size = hex_to_dec(data_section->substr(0, next_space).c_str(), (unsigned int) data_section->size() - 1);
 
     get_pcb(ID)->set_data_section(input_size, output_size, temp_size);
 }
@@ -109,6 +109,11 @@ pcb *pcb_control::get_pcb(unsigned int ID)
     }
 
     return nullptr;
+}
+
+pcb *pcb_control::get_next_pcb()
+{
+
 }
 
 pcb *pcb_control::get_highest_priority_pcb()
--- pcb/pcb_control.h
+++ pcb/pcb_control.h
@@ -12,9 +12,10 @@ public:
     static void update_pcb(unsigned int ID, std::string *data_section);
 
     static pcb *get_pcb(unsigned int ID);
-    static pcb *get_highest_priority_pcb();
-
     static void delete_pcb(unsigned int ID);
+
+    static pcb *get_next_pcb();
+    static pcb *get_highest_priority_pcb();
 };
 
 #endif //CS3502_PCB_CONTROL_H
