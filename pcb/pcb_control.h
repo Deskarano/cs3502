1f57c2ec8cbae5fc2f5d3c63be583b5e15950265
Drew <drew@savas.name>
2018-02-22 20:35:56 -0500

    > Moved short term sceduling to shortterm and out of pcb_control > Added ready queues to shortterm scheduling (linked list of pcb_node)

--- pcb/pcb_control.h
+++ pcb/pcb_control.h
@@ -14,13 +14,6 @@ public:
     static pcb *get_highest_priority_pcb();
     static pcb *get_next_pcb();
 
-    //used to find the correct pcb when short-term scheduling
-    static pcb *sched_firstComeFirstServe();
-    static pcb *sched_highestPriority();
-    static pcb *sched_shortestJob();
-    static pcb *sched_shortestRemaining();
-    static pcb *sched_roundRobin();
-
     static void delete_pcb(unsigned int ID);
 
 };
--- scheduler/short-term.h
+++ scheduler/short-term.h
@@ -7,6 +7,9 @@
 
 #include "../pcb/pcb_control.h"
 #include "../pcb/pcb.h"
+#include "../pcb/pcb_control.cpp"
+
+
 
 enum sched_algo
 {
@@ -20,11 +23,25 @@ enum sched_algo
 class shortterm
 {
 public:
-    shortterm(sched_algo sa);
-    pcb* get_next_process(sched_algo sa);
+    shortterm(sched_algo sa = FCFS);
+    pcb* get_next_process(int queue_index = 0);
+    void add_pcb_to_ready(pcb* new_pcb, int queue_index = 0);
+    void remove_pcb_from_ready(int pcb_id, int queue_index = 0);
     static void set_scheduling_algorithm(sched_algo sa);
 private:
-    static sched_algo scheduling_algorithm;
+    //used to find the correct pcb when short-term scheduling
+    pcb *sched_firstComeFirstServe(pcb_node *head_ptr);
+    pcb *sched_highestPriority(pcb_node *head_ptr);
+    pcb *sched_shortestJob(pcb_node *head_ptr);
+    pcb *sched_shortestRemaining(pcb_node *head_ptr);
+    pcb *sched_roundRobin(pcb_node *head_ptr);
+
+    static sched_algo scheduling_algorithm = FCFS;
+
+    //set to 4 for now, one for each core
+    const int num_queues = 4;
+    static pcb_node *ready_queue_heads[num_cores];      //store the head pointers for each queue
+    static int get_queue_length(pcb_node *head_ptr);    //get the length of the queue
 };
 
 #endif //CS3502_SHORT_TERM_H
