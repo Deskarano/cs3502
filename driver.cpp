a02397703ee722d4dd7222008c96f4bf0fe0e668
Drew Savas <asavas@students.kennesaw.edu>
2018-03-22 17:51:45 -0400

    Testing on VM - 3

--- driver.cpp
+++ driver.cpp
@@ -67,8 +67,8 @@ int main()
 
         if(longterm::pcbs_left_ram() == 0 && longterm::pcbs_left_total() > 0)
         {
-            //longterm::schedule_priority();
-            longterm::schedule_fcfs();
+            longterm::schedule_priority();
+            //longterm::schedule_fcfs();
             log_status::dump_ram();
         }
 
