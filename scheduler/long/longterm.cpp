ab7d175008e57c1088c72c67c9e0e6515f95d97d
Drew Savas <asavas@students.kennesaw.edu>
2018-03-22 18:31:31 -0400

    Testing on VM - Priority Testing - Fixed priorities

--- scheduler/long/longterm.cpp
+++ scheduler/long/longterm.cpp
@@ -55,7 +55,7 @@ void longterm::create_pcb(std::string *job_section, std::string *data_section, u
     job_section->erase(0, next_space + 1);
     next_space = (int) job_section->find(' ');
     priority = hex_to_dec(job_section->substr(0, job_section->size()).c_str(), (unsigned int) job_section->size()-1);
-
+ 
     //analyze data_section
     unsigned int input_size, output_size, temp_size;
 
